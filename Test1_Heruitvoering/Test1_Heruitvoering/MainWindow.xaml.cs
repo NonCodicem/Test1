7ff57d55e203fcb4a1fb03f94443141168263e95
Serge Fabre <serge.fabre@intecbrussel.be>
2019-04-03 16:09:20 +0200

    Problemen met permissies op folders

--- Test1_Heruitvoering/Test1_Heruitvoering/MainWindow.xaml.cs
+++ Test1_Heruitvoering/Test1_Heruitvoering/MainWindow.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
+using System.IO;
 
 namespace Test1_Heruitvoering
 {
@@ -20,9 +21,57 @@ namespace Test1_Heruitvoering
     /// </summary>
     public partial class MainWindow : Window
     {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        //string folderPath = @"D:\BACHELOR\2018-2019\AD 2\test1 opgave\";
+        string folderPakbonnen = "pakbonnen";
+
         public MainWindow()
         {
             InitializeComponent();
+            MIExporteren.IsEnabled = false;
+        }
+
+        public void AfsluitenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+
+        public void ImporterenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            string lokatie = Path.Combine(folderPath, folderPakbonnen);
+            var filenames = Directory.GetFiles(lokatie).Select(System.IO.Path.GetFileName);
+            foreach (var item in filenames)
+            {
+                lstBestandslijst.Items.Add(item);
+            }
+
+        }
+
+        public void ExporterenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void LstBestandslijst_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string lokatie = Path.Combine(folderPath, folderPakbonnen);
+            string bestanden = Path.Combine(lokatie, Convert.ToString(lstBestandslijst.SelectedItem).Trim());
+
+
+            using (StreamReader inputstream = File.OpenText(lokatie))
+            {
+                string line;
+                while ((line = inputstream.ReadLine()) != null)
+                {
+                    txtInhoudBestand.AppendText(line);
+                    txtInhoudBestand.AppendText(Environment.NewLine);
+
+                }
+
+            }
+            btnVerwerk.IsEnabled = true;
         }
     }
 }
